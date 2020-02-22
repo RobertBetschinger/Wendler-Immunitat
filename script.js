0c446506ae8680a9bd15be94f11735ead5d13bb1
Robert Betschinger <robert-betschinger@outlook.de>
2020-02-22 22:03:45 +0100

    nur bild

--- script.js
+++ script.js
@@ -3,32 +3,14 @@ replaceText(document.body)
 function replaceText(element){
     if(element.hasChildNodes()){
         element.childNodes.forEach(replaceText)
-    } else if(element.nodeType === Text.TEXT_NODE){
-        if(element.textContent.match(/Wendler/gi)){
-          element.parentElement.remove()
-        }
-        else if(element.textContent.match(/Laura Müller/gi)){
-            element.parentElement.remove()
-          }
-        else  if(element.textContent.match(/Heidi Klum/gi)){
-            element.parentElement.remove()
-          }
-        else  if(element.textContent.match(/Bachelor/gi)){
-            element.parentElement.remove()
-          }
-        
+    } else if(element.nodeType === Node.TEXT_NODE){
+      //console.log("Text Found! textContent: ",element.textContent)
     }
-    else if(element.nodeType === "IMG"){
-        if(element.src.match(/Wendler/gi)){
-            element.remove()
-        }
-        if(element.alt.match(/Wendler/gi)){
-            element.remove()
-        }
-        if(element.title.match(/Wendler/gi)){
-            element.remove()
-        }
-
+    else if(element.nodeName = "IMG"){
+      console.log("Image Found! Alt: ",element.alt)
+      let re = /Wendler/gi
+      if(element.alt.match(re) || element.src.match(re) || element.title.match(re)){
+        element.remove()
+      }
     }
-    
 }
